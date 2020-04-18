dfbd0400d80faef9b1459c8a5b5c7f468739af3b
iashchuk <221box@list.ru>
2020-04-18 18:28:26 +0300

    Реализует запуск приложения на express

---
+++ src/express/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require(`express`);
+const offersRoutes = require(`./routes/offers-routes`);
+const myRoutes = require(`./routes/my-routes`);
+const mainRoutes = require(`./routes/main-routes`);
+
+const DEFAULT_PORT = 8080;
+
+const app = express();
+
+app.use(`/offers`, offersRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/`, mainRoutes);
+
+app.listen(DEFAULT_PORT, () => {
+  console.log(`App listening on port 8000!`);
+});
